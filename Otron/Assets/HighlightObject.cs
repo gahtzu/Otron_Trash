d745f0e9af6f4adda97b5ad0a550bb641315cada
valdeezzee <jvalderrama91@knights.ucf.edu>
2019-01-19 09:41:19 -0500

    adding project

---
+++ Otron/Assets/HighlightObject.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+[System.Serializable]
+public class HighlightObject {
+
+    [HideInInspector]
+    public bool outLine = true, canHighlight = true;
+    public Material[] ogMaterials, newMaterials;
+    private Material highlightAll, highlightOutline, highlightCustom;
+    private MeshRenderer meshRenderer;
+    private bool highlighted = false;
+
+    private Material customHighlight;
+
+    public HighlightObject(Transform obj, Color col, Material custom = null)
+    {
+        highlightCustom = custom;
+
+        Material highlightMat = Resources.Load("HighlightObject") as Material;
+        
+        highlightOutline = highlightMat;
+
+        meshRenderer = obj.GetComponent<MeshRenderer>();
+        if(meshRenderer != null)
+        {
+            ogMaterials = meshRenderer.materials;
+            InitializeNewMaterials(col);
+        }
+    }
+
+    public void Highlight()
+    {
+        if (!highlighted && canHighlight)
+        {
+            meshRenderer.materials = newMaterials;
+            highlighted = true;
+        }
+    }
+
+    public void UnHighlight()
+    {
+        if (highlighted && canHighlight)
+        {
+            meshRenderer.materials = ogMaterials;
+            highlighted = false;
+        }
+    }
+
+    private void InitializeNewMaterials(Color col)
+    {
+        int materialsCount = ogMaterials.Length;
+        newMaterials = new Material[materialsCount + 1];
+        for (int i = 0; i < materialsCount; i++)
+        {
+            Material mat = ogMaterials[i];
+            newMaterials[i] = mat;
+        }
+
+        Material dupliMat = new Material(highlightOutline);        
+        dupliMat.SetColor("_OutlineColor",col);        
+        dupliMat.name = "DupliMat";
+
+        newMaterials[materialsCount] = highlightCustom != null ? highlightCustom : outLine ? dupliMat : highlightAll;
+        canHighlight = true;
+    }
+}
---
+++ Otron/Assets/Highlighting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlighting : MonoBehaviour
+{
+    private HighlightObject highlight;
+    public Color highlightColor;
+    // Use this for initialization
+    // Update is called once per frame
+    void Update()
+    {
+        #region Debugging
+        //if (Input.GetKeyDown(KeyCode.P))
+        //{
+        //    Highlight();
+        //}
+        //if (Input.GetKeyDown(KeyCode.O))
+        //{
+        //    UnHighlight();
+        //}
+        #endregion
+    }
+
+    void Awake()
+    {
+        highlight = new HighlightObject(transform, highlightColor);
+    }
+
+    public void Highlight()
+    {
+        if (highlight != null)
+            highlight.Highlight();
+    }
+
+    public void UnHighlight()
+    {
+        if (highlight != null)
+            highlight.UnHighlight();
+    }
+}
